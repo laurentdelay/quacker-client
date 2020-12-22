2686bcf67a7d91afad0a637bd738cdeaef0efccc
laurentdelay <laurent.delay89@gmail.com>
2020-12-22 21:12:00 +0100

    suppresion console log inutile

--- src/Apollo.js
+++ src/Apollo.js
@@ -10,7 +10,6 @@ import { setContext } from "@apollo/client/link/context";
 import App from "./App";
 
 const SERVER_URI = process.env.REACT_APP_SERVER_URI;
-console.log(SERVER_URI);
 
 export default function Apollo() {
   const httpLink = createHttpLink({
